955687ed3c0173f0df5a871b19360e4a0b0cf8cf
Muhammad Aryaputra Wirawan <143393183+AryaWirawwn@users.noreply.github.com>
2024-03-04 09:08:20 +0800

    Update MinPro2.py
    Menambahkan fitur tambah di awal, tengah dan akhir.

--- MinPro2.py
+++ MinPro2.py
@@ -15,7 +15,30 @@ class ManajemenKeyboard():
         self.head = None
     
     def tambahKeyboard(self, idKeyboard, pcb, case, switch, keyCaps, stabilizer):
-        nodeBaru = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+        if self.head == None:
+            self.head = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+        else:
+            new_node = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+            new_node.next = self.head
+            self.head = new_node
+    
+    def tambahKeyboardDariTengah(self, prev_id, idKeyboard, pcb, case, switch, keyCaps, stabilizer):
+        new_node = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+        if self.head is None:
+            print("Daftar keyboard kosong, tidak bisa menambahkan ke tengah.")
+            return
+        else:
+            current = self.head
+            while current.next:
+                if current.idKeyboard == prev_id:
+                    new_node.next = current.next
+                    current.next = new_node
+                    return
+                current = current.next
+            print("ID keyboard sebelumnya tidak ditemukan.")
+        
+    def tambahKeyboardDariTail(self, idKeyboard, pcb, case, switch, keyCaps, stabilizer):
+        new_node = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
         if self.head == None:
             self.head = NodeKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
         else:
@@ -47,7 +70,7 @@ class ManajemenKeyboard():
 
     def tampilkanKeyboard(self):
         if self.head == None:
-            print("Tidak Ada Data Keyboard")
+            print("Dafter Keyboard Kosong.")
         else:
             current = self.head
             print("Daftar Keyboard")
@@ -66,25 +89,27 @@ class ManajemenKeyboard():
                 i += 1
 
     def hapusKeyboard(self, idKeyboard):
-        if self.head == None:
+        if self.head is None:
+            print("Daftar Keyboard Kosong.")
             return
         else:
+            # Jika keyboard yang akan dihapus adalah keyboard pertama
+            if self.head.idKeyboard == idKeyboard:
+                self.head = self.head.next
+                print(f"Keyboard dengan ID {idKeyboard} berhasil dihapus.")
+                return
+
+            # Mencari keyboard yang akan dihapus
             current = self.head
-            while current is not None:
-                if current.idKeyboard == idKeyboard:
-                    if current.next is None:
-                        current = None
-                        return
-                    else:
-                        current.idKeyboard = current.next.idKeyboard
-                        current.pcb = current.next.pcb
-                        current.case = current.next.case
-                        current.switch = current.next.switch
-                        current.keyCaps = current.next.keyCaps
-                        current.stabilizer = current.next.stabilizer
-                        current.next = current.next.next
-                        return
+            while current.next is not None:
+                if current.next.idKeyboard == idKeyboard:
+                    current.next = current.next.next
+                    print(f"Keyboard dengan ID {idKeyboard} berhasil dihapus.")
+                    return
                 current = current.next
+
+            # Jika keyboard tidak ditemukan
+            print(f"Keyboard dengan ID {idKeyboard} tidak ditemukan.")
 
 def menu():
     manajemenKeyboardobj = ManajemenKeyboard()
@@ -99,21 +124,71 @@ def menu():
         print("+=========================+")
         pilihan = int(input("Masukkan Pilihan [1/2/3/4/5]: "))
         if pilihan == 1:
-            idKeyboard = input("Masukkan ID Keyboard 6 Digit: ")
-            if idKeyboard.isnumeric() and len(idKeyboard) == 6:
-                if manajemenKeyboardobj.cekIdKeyboard(idKeyboard):
-                    print("ID Sudah Ada di Database")
-                    input("Tekan enter untuk melanjutkan...")
+            os.system("cls")
+            print("+=========================+")
+            print("| [1]. Tambah Di Awal     |")
+            print("| [2]. Tambah Di Tengah   |")
+            print("| [3]. Tambah Di Akhir    |")
+            print("+=========================+")
+            pilihanTambah = int(input("Masukkan Pilihan[1/2/3]: "))
+            if pilihanTambah == 1:
+                idKeyboard = input("Masukkan ID Keyboard 6 Digit: ")
+                if idKeyboard.isnumeric() and len(idKeyboard) == 6:
+                    if manajemenKeyboardobj.cekIdKeyboard(idKeyboard):
+                        print("ID Sudah Ada di Database")
+                        input("Tekan enter untuk melanjutkan...")
+                    else:
+                        pcb = str(input("Masukkan Nama PCB: "))
+                        case = str(input("Masukkan Nama Case: "))
+                        switch = str(input("Masukkan Nama Switch: "))
+                        keyCaps = str(input("Masukkan Nama Keycaps: "))
+                        stabilizer = str(input("Masukkan Nama Stabilizer: "))
+                        manajemenKeyboardobj.tambahKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+                        input("Tekan enter untuk melanjutkan...")
                 else:
-                    pcb = str(input("Masukkan Nama PCB: "))
-                    case = str(input("Masukkan Nama Case: "))
-                    switch = str(input("Masukkan Nama Switch: "))
-                    keyCaps = str(input("Masukkan Nama Keycaps: "))
-                    stabilizer = str(input("Masukkan Nama Stabilizer: "))
-                    manajemenKeyboardobj.tambahKeyboard(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+                    print("Pastikan ID hanya angka dan 6 digit.")
                     input("Tekan enter untuk melanjutkan...")
+
+            elif pilihanTambah == 2:
+                idKeyboard = input("Masukkan ID Keyboard 6 Digit: ")
+                if idKeyboard.isnumeric() and len(idKeyboard) == 6:
+                    if manajemenKeyboardobj.cekIdKeyboard(idKeyboard):
+                        print("ID Sudah Ada di Database")
+                        input("Tekan enter untuk melanjutkan...")
+                    else:
+                        pcb = str(input("Masukkan Nama PCB: "))
+                        case = str(input("Masukkan Nama Case: "))
+                        switch = str(input("Masukkan Nama Switch: "))
+                        keyCaps = str(input("Masukkan Nama Keycaps: "))
+                        stabilizer = str(input("Masukkan Nama Stabilizer: "))
+
+                        prev_id = input("Masukkan ID Keyboard Sebelumnya: ")
+                        manajemenKeyboardobj.tambahKeyboardDariTengah(prev_id, idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+                        input("Tekan enter untuk melanjutkan...")
+                else:
+                    print("Pastikan ID hanya angka dan 6 digit.")
+                    input("Tekan enter untuk melanjutkan...")
+
+            elif pilihanTambah == 3:
+                idKeyboard = input("Masukkan ID Keyboard 6 Digit: ")
+                if idKeyboard.isnumeric() and len(idKeyboard) == 6:
+                    if manajemenKeyboardobj.cekIdKeyboard(idKeyboard):
+                        print("ID Sudah Ada di Database")
+                        input("Tekan enter untuk melanjutkan...")
+                    else:
+                        pcb = str(input("Masukkan Nama PCB: "))
+                        case = str(input("Masukkan Nama Case: "))
+                        switch = str(input("Masukkan Nama Switch: "))
+                        keyCaps = str(input("Masukkan Nama Keycaps: "))
+                        stabilizer = str(input("Masukkan Nama Stabilizer: "))
+                        manajemenKeyboardobj.tambahKeyboardDariTail(idKeyboard, pcb, case, switch, keyCaps, stabilizer)
+                        input("Tekan enter untuk melanjutkan...")
+                else:
+                    print("Pastikan ID hanya angka dan 6 digit.")
+                    input("Tekan enter untuk melanjutkan...")
+            
             else:
-                print("Pastikan ID hanya angka dan 6 digit.")
+                print("Pilihan Tidak Ada.")
                 input("Tekan enter untuk melanjutkan...")
         
         elif pilihan == 2:
@@ -137,10 +212,10 @@ def menu():
             input("Tekan enter untuk melanjutkan...")
         
         elif pilihan == 4:
+            os.system('cls')
             idKeyboard = input("Masukkan ID Keyboard: ")
             if manajemenKeyboardobj.cekIdKeyboard(idKeyboard):
                 manajemenKeyboardobj.hapusKeyboard(idKeyboard)
-                print(f"Keyboard dengan ID {idKeyboard} berhasil dihapus.")
                 input("Tekan enter untuk melanjutkan...")
             else:
                 print(f"Keyboard dengan ID {idKeyboard} Tidak Ada di Database.")
@@ -148,5 +223,9 @@ def menu():
         
         elif pilihan == 5:
             break
+
+        else:
+            print("Pilihan Tidak Ada.")
+            input("Tekan enter untuk melanjutkan...")
 
 menu()
